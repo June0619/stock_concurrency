6ac7767ee14320e0b7808be946c5710e3ce9e896
June0619 <jjw5050@hotmail.com>
2025-02-04 18:24:55 +0900

    synchronized 를 이용한 해결

--- src/main/java/me/jwjung/stock/service/StockService.java
+++ src/main/java/me/jwjung/stock/service/StockService.java
@@ -3,6 +3,7 @@ package me.jwjung.stock.service;
 import me.jwjung.stock.domain.Stock;
 import me.jwjung.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -13,7 +14,9 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    public void decrease(Long id, Long quantity) {
+    //Transactional 애노테이션은 proxy 객체로 만들기 떄문에 synchronized 를 사용할 수 없음
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         // 1. Stock 조회
         // 2. 재고 감소 후
         // 3. 갱신된 값을 저장
